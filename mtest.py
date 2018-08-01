6f27efe57a3f281059d672c398a7178b04f47949
szonglong <E0004621@u.nus.edu>
2018-08-01 17:15:47 +0800

    1.3
    Changed the smoothing to custom running mean. Kinda works now

--- mtest.py
+++ mtest.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
+#from scipy.signal import savgol_filter
 import Tkinter
 import tkFileDialog
 
@@ -20,7 +20,7 @@ area=4.29e-6
 ############ File Search ############
 root = Tkinter.Tk()
 root.withdraw() #use to hide tkinter window
-
+#
 currdir = os.getcwd()
 tempdir = tkFileDialog.askdirectory(parent=root, initialdir='C:\\Users\\E0004621\\Desktop\\Zong Long\\Papers\\Data', title='Please select the data folder') #select directory for data
 #tempdir = 'C:\\Users\\E0004621\\Desktop\\Pythontest' #Debugging use
@@ -42,17 +42,25 @@ print wlist    #is shown on command prompt dialogue
 
 ############ Processing data ############
 def ana_var(sheet_name):
-    j = np.array([0]*len(dat1.Vs))
-    
+    j = np.array([0]*len(dat1.Vs),dtype=float)
     for i in range(len(dat2['j%s'%sheet_name])):
         j[i] = dat2['j%s'%sheet_name][i]
-    j_hat = savgol_filter(j, 51 if len(j)>51 else (len(j)/2)*2-1, 3)
+    j_hat=smooth(j,4)   #self degined running median smoothing
     dev = np.gradient(np.array(dat1.Vs),j_hat)
     m0 = np.multiply(np.multiply(j, dev),1/(0.0259*T/300))
-    m_hat = savgol_filter(m0, 51 if len(m0)>51 else (len(j)/2)*2-1, 3)
+    m_hat = smooth(m0,1)
     dev_df['m%s' %sheet_name] = m_hat
+    smoothed_df['smoothed j%s' %sheet_name] = np.abs(j)
     return
 
+def smooth(iterable,repeats):
+    for t in range(repeats):
+        for i in range(len(iterable)):
+            if i in [0,1,len(iterable)-2,len(iterable)-1]:
+                pass
+            else:
+               iterable[i] = (0.25*(iterable[i-2]+iterable[i+2])+0.5*(iterable[i+1]+iterable[i-1])+iterable[i])/2.5
+    return iterable
 for file in wlist:
     wb=pd.read_excel(r'%s.xls' %file, None)
 
@@ -63,6 +71,7 @@ for file in wlist:
             sheet_name = '1'
             dat1=pd.DataFrame({'Vs': sh.Vs[0:len(sh.Vs)/4]})
             dev_df = dat1.copy()
+            smoothed_df = dev_df.copy()
             dat2=pd.DataFrame({'j1': sh.Id[0:len(sh.Vs)/4]/(10*area)*d}) # ln current density in mA/cm2
             plot_array = dat1.join(np.abs(dat2))
 
@@ -74,17 +83,19 @@ for file in wlist:
         m_list=ana_var(sheet_name)
 ############ Plotting settings ############
 
-        plt.figure(1)    
-        fig, ax = plt.subplots()
-        ax2 = ax.twinx()
-        dev_df.plot(x='Vs',ax=ax, ylim=plotyrange, figsize=(11,11))
-        plot_array.plot(x='Vs',ax=ax2, ylim=plotyrange2, logy=True, ls='--')
-        plt.xlim(plotxrange)
-        plt.xlabel('Applied voltage(V)')
-        ax.set_ylabel('Local Ideality factor')
-        ax2.set_ylabel('Current density(mAcm$^-$$^2$)')
-        ax2.legend(loc=0)
-        font={'size':18}
-        plt.rc('font',**font)
-        plt.savefig('processed_ideality/%s _m.png' %file)
-        plt.close()
+    plt.figure(1)    
+    fig, ax = plt.subplots()
+    ax2 = ax.twinx()
+    dev_df.plot(x='Vs',ax=ax, ylim=plotyrange, figsize=(11,11))
+    smoothed_df.plot(x='Vs',ax=ax2, ylim=plotyrange2, logy=True, ls='--')
+    plot_array.plot(x='Vs',ax=ax2, ylim=plotyrange2, logy=True, ls=':')
+    plt.xlim(plotxrange)
+    plt.xlabel('Applied voltage(V)')
+    ax.set_ylabel('Local Ideality factor')
+    ax2.set_ylabel('Current density(mAcm$^-$$^2$)')
+    ax.legend(loc=0)
+    ax2.legend(loc='lower right')
+    font={'size':18}
+    plt.rc('font',**font)
+    plt.savefig('processed_ideality/%s _m.png' %file)
+    plt.close()
